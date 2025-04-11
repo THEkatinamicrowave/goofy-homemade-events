2ae39ce0eb137454737be47fc2bda84e57323b0a
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-04-10 20:15:05 -0400

    re-added original stage events too

--- data/stages/school.hx
+++ data/stages/school.hx
@@ -1,6 +1,14 @@
 //
 var girlsPos:FlxPoint = FlxPoint.get(-100, 190);
 
+if (PlayState.SONG.meta.name.toLowerCase() == "roses") {
+	bgGirls.animation.remove("danceLeft");
+	bgGirls.animation.remove("danceRight");
+	bgGirls.animation.addByIndices('danceLeft', 'BG fangirls dissuaded', CoolUtil.numberArray(14), "", 24, false);
+	bgGirls.animation.addByIndices('danceRight', 'BG fangirls dissuaded', CoolUtil.numberArray(30, 15), "", 24, false);
+}
+bgGirls.animation.play("danceLeft", true); // horrible fix, please fix later
+
 function beatHit(beat:Int) {
 	var dir:Int = switch (beat % 2) {
 		case 0:
