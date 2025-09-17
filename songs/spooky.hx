7e92773466b0e532f6e00c1c738559c92ff3e3c8
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-09-16 21:25:59 -0400

    spooky is EPICVCCCCC

--- songs/spooky.hx
+++ songs/spooky.hx
@@ -1,12 +1,27 @@
 //
+var removed:Bool = false;
+
 function onCountdown(event:CountdownEvent) {
+    removed = false;
     if (dad.curCharacter == "spooky") for (strum in get_cpu().members) strum.scrollSpeed = 15;
 }
 
 function postUpdate(elapsed:Float) {
-    if (strumLines.members[curCameraTarget] == get_cpu()) {
+    if (dad.curCharacter == "spooky" && strumLines.members[curCameraTarget] == get_cpu()) {
         var newNote:Note = new Note(get_cpu(), {time: inst.time + 100, id: FlxG.random.int(0, 3), type: 0, sLen: 0}, false, 0, 0);
-
         get_cpu().notes.addNotes([newNote]);
+    }
+}
+
+function onDadHit(event:NoteHitEvent) {
+    if (dad.curCharacter == "spooky" && FlxG.random.bool(0.1)) {
+        removed = true;
+        dad.visible = false;
+        get_cpu().visible = false;
+    }
+
+    if (removed) {
+        vocals.volume = 0;
+        event.preventVocalsUnmute();
     }
 }
